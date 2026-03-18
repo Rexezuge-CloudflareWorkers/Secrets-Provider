3138ebdaf2ce59fd8b912bbf0354fa5332fb1d26
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2026-03-18 01:54:59 +0000

    FEAT: provides cloudflare and tailscale

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+  async fetch(request: Request, env: Env, _ctx: never) {
+    const authHeader = request.headers.get('Authorization');
+
+    const expectedToken: string = await env.BEARER_TOKEN_SECRET.get();
+    const expectedAuthorization = `Bearer ${expectedToken}`;
+
+    if (authHeader !== expectedAuthorization) {
+      return new Response('Unauthorized', { status: 401 });
+    }
+
+    const url: URL = new URL(request.url);
+    if (url.pathname === '/tailscale') {
+      return new Response(await env.TAILSCALE_AUTH_KEY_SECRET.get(), { status: 200 });
+    }
+    if (url.pathname === '/cloudflare') {
+      return new Response(await env.CLOUDFLARE_TUNNEL_TOKEN_SECRET.get(), { status: 200 });
+    }
+    return new Response('Not Found', { status: 404 });
+  },
+};
